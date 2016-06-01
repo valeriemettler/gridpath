7c248bc649531cf80d5ca6794d2a0d7c0303a63c
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-31 21:11:07 -0700

    added css file and dynamically generated a box with javascript

--- js/gridpath.js
+++ js/gridpath.js
@@ -1,12 +1,13 @@
 //Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 //How many such routes are there through a 20×20 grid?
 debugger;
-var gridPath = function() {
-
+var grid_path = function() {
+    var g = '<div class="box"></div>';
+    $('#x').html(g);
 
 };
 
 var test = function(){
-  gridPath();
+  grid_path();
 }
 test();
