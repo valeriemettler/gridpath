290e2e48923d8ee863df561ceb7ce7bb2fb4f23c
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-31 22:48:09 -0700

    created possible outputs positions and variables in an array

--- js/gridpath.js
+++ js/gridpath.js
@@ -1,13 +1,23 @@
 //Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 //How many such routes are there through a 20×20 grid?
 debugger;
-var grid_path = function() {
-    var g = '<div class="box"></div>';
-    $('#x').html(g);
+var grid_path = function(dimen) {
+    // var g = '<div class="box"></div>';
+    // $('#x').html(g);
+    //r = right
+    //d = down
+    var possible_outputs_array = [];
+    possible_outputs_array = "rd".repeat(dimen).split('');
+    console.log(possible_outputs_array);
+
 
 };
 
 var test = function(){
-  grid_path();
+  var dimension = [
+    2,
+
+  ];
+  grid_path(dimension);
 }
 test();
